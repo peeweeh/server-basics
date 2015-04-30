95cc3a0fde218c264096c6b6cb03b727a97f0b77
Paul Hidalgo <me@paulhidalgo.net>
2015-04-30 15:17:15 +0800

    Get Configs

--- get_configs.sh
+++ get_configs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 source /var/server-basics/get_aws_info.sh
-aws s3 cp s3://$VPC/config/server/install-detailed.sh /var/install-detailed.sh
+aws s3 cp s3://$VPC/config/server/download_devops.sh /var/download_devops.sh
