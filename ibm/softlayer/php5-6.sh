9bda2c9b0570d5bd6e4c7f3c380467419dcddef6
Paul Hidalgo <me@paulhidalgo.net>
2015-08-03 10:16:16 +0800

    Changed PHP and Added Github

--- ibm/softlayer/php5-6.sh
+++ ibm/softlayer/php5-6.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
-rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm | tee -a /var/log/install-scripts.log
+rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm | tee -a /var/log/install-scripts.log
+yum install -y git | tee -a /var/log/install-scripts.log
+git clone https://github.com/peeweeh/server-basics.git /var/server-basics | tee -a /var/log/install-scripts.log
+sh /var/server-basics/general/networking/iptables-rules.sh
 
 ln -sf /usr/share/zoneinfo/Asia/Singapore /etc/localtime
 yum update -y | tee -a /var/log/install-scripts.log
 
-yum install -y gcc-c++ pcre-dev pcre-devel zlib-devel make jq npm awslogs | tee -a /var/log/install-scripts.log
+yum install -y git gcc-c++ pcre-dev pcre-devel zlib-devel make jq npm awslogs | tee -a /var/log/install-scripts.log
 yum install -y mysql nginx memcached  | tee -a /var/log/install-scripts.log
-yum install -y php56w php56w-fpm php56w-mysql php56w-gd php56w-xml php56w-odbc php56w-imap php56w-mcrypt php56w-soap php56w-mbstring php56w-pecl-memcache php56w-mysqlnd php56w-pdo
+yum install -y php56w php56w-fpm php56w-mysql php56w-gd php56w-xml php56w-odbc php56w-imap php56w-mcrypt php56w-soap php56w-mbstring php56w-pecl-memcache php56w-pdo | tee -a /var/log/install-scripts.log
 yum install -y php-pear  | tee -a /var/log/install-scripts.log
 yum install -y nc telnet htop svn perl-libwww-perl pinentry rng-tools| tee -a /var/log/install-scripts.log
 
