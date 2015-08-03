47d5a43743a45fe8e5d00d6d81961608c0e02f4d
Paul Hidalgo <me@paulhidalgo.net>
2015-08-03 09:31:27 +0800

    PHP 5.6

---
+++ ibm/softlayer/php5-6.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+yum install -y php56w php56w-fpm php56w-gd php56w-mbstring php56w-pdo php56w-xml
+ln -sf /usr/share/zoneinfo/Asia/Singapore /etc/localtime
+yum update -y | tee -a /var/log/install-scripts.log
+
+yum install -y gcc-c++ pcre-dev pcre-devel zlib-devel make jq npm awslogs | tee -a /var/log/install-scripts.log
+yum install -y mysql nginx memcached  | tee -a /var/log/install-scripts.log
+yum install -y php56w php56w-fpm php56w-mysql php56w-gd php56w-xml php56w-odbc php56w-imap php56w-mcrypt php56w-soap php56w-mbstring php56w-pecl-memcache php56w-mysqlnd php56w-pdo
+yum install -y php-pear  | tee -a /var/log/install-scripts.log
+yum install -y nc telnet htop svn perl-libwww-perl pinentry rng-tools| tee -a /var/log/install-scripts.log
+
+chkconfig nginx on
+chkconfig httpd on
+
+echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
