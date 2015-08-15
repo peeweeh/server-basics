cfb9061f078c85c386158cb60a5cf0a1bc5a74f2
Paul Hidalgo <me@paulhidalgo.net>
2015-08-15 21:15:54 +0800

    Adjustments on NGINX

--- general/nginx/nginx-repo.sh
+++ general/nginx/nginx-repo.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 echo "********* Install Nginx Packages ********"
-echo "[nginx]" > /etc/yum.repos.d/nginx.repo
-echo "name=nginx repo" >> /etc/yum.repos.d/nginx.repo
-echo "baseurl=http://nginx.org/packages/rhel/$releasever/$basearch/" >> /etc/yum.repos.d/nginx.repo
-echo "gpgcheck=0" >> /etc/yum.repos.d/nginx.repo
-echo "enabled=1" >> /etc/yum.repos.d/nginx.repo
+cp nginx.repo /etc/yum.repos.d/nginx.repo
