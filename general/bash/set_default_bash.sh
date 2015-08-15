1f32467f11281688191e62a8dd6e1b2431a9004b
Paul Hidalgo <me@paulhidalgo.net>
2015-08-15 21:16:04 +0800

    Adjustments for Bash

--- general/bash/set_default_bash.sh
+++ general/bash/set_default_bash.sh
@@ -3,6 +3,3 @@ echo "********* Set Default Bash Script ********"
 echo "if [ -f /var/server-basics/general/bash/bash_profile ]; then" >> /etc/profile
 echo " . /var/server-basics/general/bash/bash_profile" >> /etc/profile
 echo "fi" >> /etc/profile
-echo "if [ -f /var/server-basics/general/bash/bash_profile ]; then" >> /root/.bashrc
-echo " . /var/server-basics/general/bash/bash_profile" >> /root/.bashrc
-echo "fi" >> /root/.bashrc
