412ca71ac488a8ab6f51c2841c6adb502a764e2f
Paul Hidalgo <me@paulhidalgo.net>
2015-08-15 16:27:32 +0800

    Shell Improvements

--- ibm/softlayer/php5-6.sh
+++ ibm/softlayer/php5-6.sh
@@ -9,12 +9,12 @@ sh /var/server-basics/general/bash/set_default_bash.sh
 ln -sf /usr/share/zoneinfo/Asia/Singapore /etc/localtime
 #yum -y -x 'kernel*' update | tee -a /var/log/install-scripts.log
 
-yum install -y git gcc-c++ pcre-dev pcre-devel zlib-devel make jq npm awslogs | tee -a /var/log/install-scripts.log
+yum install -y python-devel libffi-devel git gcc-c++ pcre-dev pcre-devel zlib-devel make jq npm awslogs | tee -a /var/log/install-scripts.log
 yum install -y mysql nginx memcached  | tee -a /var/log/install-scripts.log
-yum install -y php56w php56w-fpm php56w-mysql php56w-gd php56w-xml php56w-odbc php56w-imap php56w-mcrypt php56w-soap php56w-mbstring php56w-pecl-memcache php56w-pdo | tee -a /var/log/install-scripts.log
+yum install -y php56w php56w-fpm php56w-mysql php56w-gd php56w-xml php56w-odbc php56w-imap php56w-mcrypt php56w-soap php56w-mbstring php56w-pecl-memcache php56w-pecl-memcached php56w-pdo | tee -a /var/log/install-scripts.log
 yum install -y php-pear  | tee -a /var/log/install-scripts.log
-yum install -y nc telnet htop svn perl-libwww-perl pinentry rng-tools| tee -a /var/log/install-scripts.log
-
+yum install -y nc telnet htop svn perl-libwww-perl pinentry rng-tools python-pip | tee -a /var/log/install-scripts.log
+pip install pyopenssl ndg-httpsclient pyasn1 python-swiftclient python-novaclient
 chkconfig nginx on
 chkconfig httpd on
 
