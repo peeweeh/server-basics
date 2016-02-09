4bc8d79f8e53e11df46e0d5464a50e2b57bc30d4
Paul Hidalgo <me@paulhidalgo.net>
2016-02-09 20:57:42 +0800

    Modified Logging and Added PHP 5.6

--- amazon/ec2/log_to_s3.sh
+++ amazon/ec2/log_to_s3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /var/development-operations/general/shell/get_server_info.sh
+source /var/server-basics/get_aws_info.sh
 aws s3 sync /var/log/ s3://$VPC/log/$YEAR/$MONTH/$HOSTNAME-$IID/ --region=$EC2_REGION --exact-timestamps
