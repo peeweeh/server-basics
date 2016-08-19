c88c4ebdbb0883cecd4313ad01a140422bdccfbf
Paul Hidalgo <me@paulhidalgo.net>
2016-08-19 15:47:15 +0800

    Import only users with Public Key

--- amazon/ec2/ssh/import_users.sh
+++ amazon/ec2/ssh/import_users.sh
@@ -5,6 +5,7 @@ aws iam list-users --query "Users[].[UserName]" --output text | while read User;
   else
   	aws iam list-ssh-public-keys --user-name "$User" --query "SSHPublicKeys[?Status == 'Active'].[SSHPublicKeyId]" --output text | while read KeyId; do
 	  /usr/sbin/adduser "$User"
+	  echo "Adding User $User"
   	  echo "$User ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/$User"
   	  break
 	done
