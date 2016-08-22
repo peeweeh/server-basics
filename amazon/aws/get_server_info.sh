a29daa218bec5be5ab24a1626f42644fd50fad3d
Paul Hidalgo <me@paulhidalgo.net>
2016-08-22 11:16:27 +0800

    Set Instance Hostname based on AWS Name Tag

--- amazon/aws/get_server_info.sh
+++ amazon/aws/get_server_info.sh
@@ -11,6 +11,9 @@ export DAY=$(date +"%d")
 export MONTH=$(date +"%m")
 export HOUR=$(date +"%H")
 export SUBNET=$(GET http://169.254.169.254/latest/meta-data/network/interfaces/macs/$MAC/subnet-id)
+export NAME=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$IID" --region=$EC2_REGION | jq -r '.Tags[] | ( if .Key == "Name" then  .Value else  "" end )' )
+
+
 if [[ ! -z "$IID" ]]; then
 	AUTO_SCALING_GROUP=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$IID" --region=$EC2_REGION | jq -r '.Tags[] | ( if .Key == "aws:autoscaling:groupName" then  .Value else  "" end )' )
 fi
