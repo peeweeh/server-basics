2c837938062b0bc79bd0531f1510bbf657aa0b1d
Paul <paul@scarletemerald.com>
2016-12-19 13:34:49 +0800

    Fixed Bash Script

--- amazon/setup/base.sh
+++ amazon/setup/base.sh
@@ -2,4 +2,4 @@
 sh /var/server-basics/amazon/ec2/ssh/setup.sh
 sh /var/server-basics/amazon/ec2/install_ssm.sh
 sh /var/server-basics/amazon/aws/system/set_hostname_from_instance_name.sh
-sh /var/server-basics/amazon/aws/general/bash/set_default_bash.sh
+sh /var/server-basics/general/bash/set_default_bash.sh
